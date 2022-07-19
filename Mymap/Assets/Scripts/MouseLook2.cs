1806e0721b35c04e5bf0c4610691199220d8362a
h4iha <attethys@gmail.com>
2022-07-19 13:09:27 +0700

    01

---
+++ Mymap/Assets/Scripts/MouseLook2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook2 : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float xRotation;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        float mouseX = 0;
+        float mouseY = 0;
+
+        if (Mouse.current != null) {
+            mouseX = Mouse.current.delta.ReadValue().x;
+            mouseY = Mouse.current.delta.ReadValue().y;
+        }
+
+        if (Gamepad.current != null) {
+            mouseX = Gamepad.current.rightStick.ReadValue().x;
+            mouseY = Gamepad.current.rightStick.ReadValue().y;
+        }
+
+        xRotation -= mouseY * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -80, 80);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX * Time.deltaTime);
+    }
+}
